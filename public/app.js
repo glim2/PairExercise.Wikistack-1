58cb3003ffc1cc3322dc3f2a6781ce18620b60e8
Finley Matthews <finleymatthews96@gmail.com>
2021-04-29 13:00:03 -0400

    almost done with sequelize essentials

---
+++ public/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const layout = require('../views/layout')
+const { db, Page, User } = require('../models')
+
+// db.authenticate()
+//   .then(() => {
+//     console.log('connected to the database');
+//   })
+
+async function init () {
+  try {
+    await db.sync({force: true});
+  } catch (error) {
+
+  }
+}
+
+// logging
+app.use(morgan('dev'));
+
+// static
+app.use(express.static(__dirname + '/public'));
+
+// body parsing
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.send((layout('')));
+})
+
+const PORT = 1337
+app.listen(PORT, () => {
+  console.log('hi there')
+});
